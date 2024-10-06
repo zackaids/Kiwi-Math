2bbf9d58bf389cfa2ed93cbbcdc09c84023daefe
zackaids <zalkar.ai77@gmail.com>
2024-10-06 08:53:05 -0400

    added logo

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 import './Home.css'; 
+import logo from './kiwilearn.png'; // Make sure to replace with the actual path to your logo image
 
 export function Home() {
   const navigate = useNavigate();
@@ -13,13 +14,25 @@ export function Home() {
     navigate('/learn');
   };
 
+  const handleProfile = () => {
+    navigate('/profile');
+  };
+
+  const handleOptions = () => {
+    navigate('/options');
+  };
+
   return (
     <div className="home-container">
+      <div className="logo-container">
+        <img src={logo} alt="Logo" className="logo" />
+        <h1 className="kiwimath-title">kiwimath</h1>
+      </div>
       <div className="home-box">
         <div className="home-button learn-button" onClick={handleLearn}>LEARN</div>
         <div className="home-button test-button" onClick={handleRetakeTest}>ASSESSMENT TEST</div>
-        <div className="home-button profile-button">PROFILE</div>
-        <div className="home-button options-button">OPTIONS</div>
+        <div className="home-button profile-button" onClick={handleProfile}>PROFILE</div>
+        <div className="home-button options-button" onClick={handleOptions}>OPTIONS</div>
       </div>
     </div>
   );
